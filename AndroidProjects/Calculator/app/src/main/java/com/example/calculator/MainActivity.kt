ac1705dbb16ec1257a342bde08e15d0702364c93
deathrack <127497796+deathrack@users.noreply.github.com>
2023-03-20 14:00:10 +0530

    2.0

--- AndroidProjects/Calculator/app/src/main/java/com/example/calculator/MainActivity.kt
+++ AndroidProjects/Calculator/app/src/main/java/com/example/calculator/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.calculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+    private var tvInput: TextView?= null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        tvInput = findViewById(R.id.tvInput)
+    }
+    fun onDigit(view: View){
+        tvInput?.append((view as Button).text)
+    }
+    fun onClear(view: View){
+        tvInput?.text = ""
     }
 }
