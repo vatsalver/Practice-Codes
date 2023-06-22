e3b6e2937a6b85b10401b04c2bef983107059a0b
deathrack <127497796+deathrack@users.noreply.github.com>
2023-06-22 20:21:21 +0530

    12.2

--- ReactJS Practice/foodapp/eatit/src/components/Card.js
+++ ReactJS Practice/foodapp/eatit/src/components/Card.js
@@ -9,9 +9,27 @@ export default function Card(props) {
   const [qty, setQty] = useState(1);
   const [size, setSize] = useState("")
   const handleAddtoCart = async () => {
+    let food = []
+    for (const item of data) {
+      if (item.id === props.foodItem._id){
+        food = item;
+
+        break;
+      }
+    }
+    if (food !== []){
+      if(food.size === size){
+        await dispatch({type: "UPDATE",id: props.foodItem._id,price:finalPrice,qty:qty})
+        return
+      }
+    else if(food.size!== size){
     await dispatch({ type: "ADD", id: props.foodItem._id, name: props.foodItem.name, price: finalPrice, qty: qty, size: size })
-    console.log(data)
+    return
+    }
+    return
   }
+  await dispatch({ type: "ADD", id: props.foodItem._id, name: props.foodItem.name, price: finalPrice, qty: qty, size: size })
+}
   let finalPrice = qty * parseInt(options[size]);
   useEffect(()  =>{
        setSize(priceRef.current.value)
--- ReactJS Practice/foodapp/eatit/src/components/contextreducer.js
+++ ReactJS Practice/foodapp/eatit/src/components/contextreducer.js
@@ -11,6 +11,16 @@ const reducer = (state,action)=>{
         let newArr = [...state];
         newArr.splice(action.index,1)
         return newArr;
+    case "UPDATE":
+      let arr = [...state]
+      arr.find((food,index) => {
+         if (food.id === action.id){
+            
+            arr[index] = {...food,qty: parseInt(action.qty) + food.qty , price : action.price + food.price}
+         }
+         return arr
+      })
+      return arr
     default:
         console.log("Error in Reducer");
         break;
